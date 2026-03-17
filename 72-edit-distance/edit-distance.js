b949e12c19c7bd9ac9c69f9053368233225292c2
Tanushri K <86180283+TANUSHRIK311@users.noreply.github.com>
2026-03-17 21:39:36 +0530

    Time: 8 ms (69.18%) | Memory: 60.6 MB (34.59%) - LeetSync

---
+++ 72-edit-distance/edit-distance.js
@@ -0,0 +1,30 @@
+var minDistance = function(word1, word2) {
+    let m = word1.length;
+    let n = word2.length;
+
+    // Create DP table
+    let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    // Base cases
+    for (let i = 0; i <= m; i++) dp[i][0] = i;
+    for (let j = 0; j <= n; j++) dp[0][j] = j;
+
+    // Fill DP table
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1]; // no operation
+            } else {
+                dp[i][j] = 1 + Math.min(
+                    dp[i - 1][j],     // delete
+                    dp[i][j - 1],     // insert
+                    dp[i - 1][j - 1]  // replace
+                );
+            }
+
+        }
+    }
+
+    return dp[m][n];
+};
